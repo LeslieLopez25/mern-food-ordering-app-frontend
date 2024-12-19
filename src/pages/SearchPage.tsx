a1bb4686e4eaca08e706b676ae2f81cc94d6156f
Leslie Lopez <leslie2515lopez@gmail.com>
2024-12-19 00:14:41 -0600

    Finished creating the pagination component

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -4,22 +4,33 @@ import SearchResultCard from "@/components/SearchResultCard";
 import SearchResultsInfo from "@/components/SearchResultsInfo";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
+import PaginationSelector from "./PaginationSelector";
 
 export type SearchState = {
   searchQuery: string;
+  page: number;
 };
 
 const SearchPage = () => {
   const { city } = useParams();
   const [searchState, setSearchState] = useState<SearchState>({
     searchQuery: "",
+    page: 1,
   });
   const { results, isLoading } = useSearchRestaurants(searchState, city);
+
+  const setPage = (page: number) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      page,
+    }));
+  };
 
   const setSearchQuery = (searchFormData: SearchForm) => {
     setSearchState((prevState) => ({
       ...prevState,
       searchQuery: searchFormData.searchQuery,
+      page: 1,
     }));
   };
 
@@ -27,6 +38,7 @@ const SearchPage = () => {
     setSearchState((prevState) => ({
       ...prevState,
       searchQuery: "",
+      page: 1,
     }));
   };
 
@@ -52,6 +64,11 @@ const SearchPage = () => {
         {results.data.map((restaurant) => (
           <SearchResultCard restaurant={restaurant} />
         ))}
+        <PaginationSelector
+          page={results.pagination.page}
+          pages={results.pagination.pages}
+          onPageChange={setPage}
+        />
       </div>
     </div>
   );
