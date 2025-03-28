9b9d561ea9edaa08d9a41c37120b735f6cc147d3
Leslie Lopez <leslie2515lopez@gmail.com>
2025-03-27 22:23:26 -0600

    Added comments to ProtectedRoute.tsx

--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -1,18 +1,19 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Navigate, Outlet } from "react-router-dom";
 
+// Protects routes by allowing access only to authenticated users
 const ProtectedRoute = () => {
   const { isAuthenticated, isLoading } = useAuth0();
 
   if (isLoading) {
-    return null;
+    return null; // Show nothing while checking authentication
   }
 
   if (isAuthenticated) {
     return <Outlet />;
   }
 
-  return <Navigate to="/" replace />;
+  return <Navigate to="/" replace />; // Redirect if not logged in
 };
 
 export default ProtectedRoute;
